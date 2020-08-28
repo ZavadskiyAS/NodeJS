5f03b5f6d37790064f715d99a483804f87ae5a58
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-08-28 23:12:19 +0300

    +listContacts

--- contacts.js
+++ contacts.js
@@ -1,11 +1,32 @@
 const fs = require('fs');
+const { promises: fsPromises } = fs;
 const path = require('path');
 
 // const contactsPath = path.parse('/e/GoIT/NodeJS/NodeJS/db/contacts.json');
 const contactsPath = path.join(__dirname, './db/contacts.json');
 console.log('contactsPath :>> ', contactsPath);
 
+// async function main() {
+//     await fs.promises.readFile('contacts.json', 'utf-8');
+//     const dirFails = await fs.promises.readdir('db');
+//     console.log('dirFails :>> ', dirFails);
+// }
+// main()
+function getData(data) {
+    return JSON.parse(data);
+  }
 
+async function listContacts() {
+    try {
+      const data = await fsPromises.readFile(contactsPath, 'utf-8');
+      console.table(getData(data));
+    } catch (err) {
+      throw err;
+    }
+  }
+
+listContacts()
+// =========================================
 // fs.writeFileSync(+файл , текст в этом файле)
 // fs.writeFile(+файл , текст в этом файле, (err) => {
     // if (err) throw new Error(err);
