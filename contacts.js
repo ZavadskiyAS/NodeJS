9e8e2fe8acbb92a72407834e82b5809c5821751c
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-08-30 19:58:20 +0300

    + addaddContact and removeContact

--- contacts.js
+++ contacts.js
@@ -7,21 +7,25 @@ const contactsPath = path.join(__dirname, './db/contacts.json');
 // console.log('contactsPath :>> ', contactsPath);
 
 // async function main() {
-//     await fs.promises.readFile('contacts.json', 'utf-8');
-//     const dirFails = await fs.promises.readdir('db');
-//     console.log('dirFails :>> ', dirFails);
-// }
-// main()
-function getData(data) {
+  //     await fs.promises.readFile('contacts.json', 'utf-8');
+  //     const dirFails = await fs.promises.readdir('db');
+  //     console.log('dirFails :>> ', dirFails);
+  // }
+  // main()
+  function getData(data) {
     return JSON.parse(data);
-}
-
-// console.log('data :>> ', data);
+  }
+  
+  function saveData(data) {
+    return JSON.stringify(data, null, 2);
+  }
+    
+  // console.log('data :>> ', data);
 
 async function listContacts() {
     try {
       const data = await fsPromises.readFile(contactsPath, 'utf-8');
-    //   console.table(getData(data));
+      console.table(getData(data));
     } catch (err) {
       throw err;
     }
@@ -34,21 +38,44 @@ async function getContactById(contactId) {
       const data = await fsPromises.readFile(contactsPath, 'utf-8');
       const listID = getData(data);
       const findItem = listID.find(item => item.id);
-      // console.table(findItem);
+      console.table(findItem);
     } catch (err) {
       throw err;
     }
   }
 getContactById()
 
-  
-function removeContact(contactId) {
-//     // ...твой код
+async function removeContact(contactId) {
+  try {
+    const data = await fsPromises.readFile(contactsPath, 'utf-8');
+    const list = getData(data);
+    const newList = list.filter(item => item.id !== contactId);
+    console.table(newList);
+    await fsPromises.writeFile(contactsPath, saveData(newList));
+  } catch (err) {
+    throw err;
+  }
 }
-  
-function addContact(name, email, phone) {
-//     // ...твой код
+removeContact();
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await fsPromises.readFile(contactsPath, 'utf-8');
+    const list = getData(data);
+    const newContact = {
+      id: list.length + 1,
+      name,
+      email,
+      phone,
+    };
+    const newList = [...list, newContact];
+    await fsPromises.writeFile(contactsPath, saveData(newList));
+    console.table(newList);
+  } catch (err) {
+    throw err;
+  }
 }
+addContact();
 
 module.exports = {
   listContacts,
